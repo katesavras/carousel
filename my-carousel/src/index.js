0c5318a7f2300008e6baf641d0b410e684fb2c9e
kate <katerina.savras@gmail.com>
2021-05-22 20:08:02 +0300

    add webpack config

---
+++ my-carousel/src/index.js
@@ -0,0 +1,6 @@
+const heading = document.createElement('h1')
+heading.textContent = 'Hello world!'
+heading.style.color = 'red'
+
+const root = document.querySelector('#root')
+root.append(heading)
---
+++ my-carousel/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+
+
+    module: {
+        rules: [
+            // JavaScript
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            // изображения
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+
+    ]
+}
